f239e9c5423db66404d6a5c03c77b1b0b0357293
peanutyumyum <jrimit@gmail.com>
2021-07-28 01:53:47 +0900

    2021. 07. 28.

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
+// require
 const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
 const path = require('path');
 
-dotenv.config();
-const app = express();
-app.set('port', process.env.PORT || 3000);
+const connect = require('/schemas');
 
-/* app.use(
-    morgan('dev'),
-    express.static('/', path.join(__dirname, 'public')), // express.static('**request dir', **'dir'));
-    express.json(),
-    express.urlencoded({extended: false}), // it can read JSON and URL-encoded
-    cookieParser(process.env.COOKIE_SECRET), // it can traslate cookies
-    ) */
+const passport = require('passport');
+
+// DB
+
+//const MongoClient = require('mongodb').MongoClient;
+
+dotenv.config();
+const passportConfig = require('./passport');
+
+const app = express();
+passportConfig();
+app.set('port', process.env.PORT || 3000);
 
 app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public'))); // app.use('**request dir', express.static(**'dir')); At this code, static file is existed in pulic folder
@@ -32,10 +37,22 @@ app.use(session({
     },
     name: 'session-cookie',
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 const multer = require('multer');
 const fs = require('fs');
 // End today work
+
+var db;
+MongoClient.connect('mongodb+srv://test:jiwon123@cluster0.nzucs.mongodb.net/todoapp?retryWrites=true&w=majority', function(error, client) {
+    if (error) {
+        return console.log(error);
+    } else {
+        db = client.db('todoapp');
+    }   
+});
+
 
 app.get('/', (req, res, next) => {
     res.send('공사중');
